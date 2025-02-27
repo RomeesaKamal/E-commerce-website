b95725b90f437f82285edf6fab4c222e62fb0ce2
RomeesaKamal <kamalromeesa@gmail.com>
2025-02-27 18:28:51 +0500

    correction

--- src/Components/ThemeToggle.js
+++ src/Components/ThemeToggle.js
@@ -2,21 +2,17 @@ import { useEffect, useState } from "react";
 import { Sun, Moon } from "lucide-react";
 
 const ThemeToggle = () => {
-  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+  const [theme, setTheme] = useState(() => localStorage.getItem("theme") || "dark");
 
   useEffect(() => {
-    if (theme === "dark") {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
+    document.documentElement.classList.toggle("dark", theme === "dark");
     localStorage.setItem("theme", theme);
   }, [theme]);
 
   return (
     <button
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-      className="sm:p-2 p-1 rounded-lg transition-all dark:hover:text-[#00FF9C] hover:text-red-900 "
+      className="sm:p-2 p-1 rounded-lg transition-all dark:hover:text-[#00FF9C] hover:text-red-900"
     >
       {theme === "light" ? <Moon size={32} /> : <Sun size={32} />}
     </button>
