c29a568e44e84961ac015ba6bae2ec3dde6602a4
RomeesaKamal <kamalromeesa@gmail.com>
2025-03-03 12:08:29 +0500

    correction

--- src/Components/ThemeToggle.js
+++ src/Components/ThemeToggle.js
@@ -5,18 +5,23 @@ const ThemeToggle = () => {
   const [theme, setTheme] = useState(() => localStorage.getItem("theme") || "dark");
 
   useEffect(() => {
-    document.documentElement.classList.toggle("dark", theme === "dark");
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
     localStorage.setItem("theme", theme);
   }, [theme]);
 
   return (
     <button
-      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
       className="sm:p-2 p-1 rounded-lg transition-all dark:hover:text-[#00FF9C] hover:text-red-900"
     >
-      {theme === "light" ? <Moon size={32} /> : <Sun size={32} />}
+      {theme === "dark" ? <Sun size={32} /> : <Moon size={32} />}
     </button>
   );
 };
 
 export default ThemeToggle;
+
